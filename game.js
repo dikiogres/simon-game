0bc0f5c27eb8f331a4777b8464928b6d09d8b200
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-02-05 21:02:06 +0700

    feat: add game start state

--- game.js
+++ game.js
@@ -1,8 +1,19 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
 var userClickedPattern = [];
+
+var started = false;
+
+var level = 0;
+
+$(document).keypress(function() {
+    if (!started) {
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  });
 
 $(".btn").click(function(){
     var userChosenColour = $(this).attr("id");
@@ -17,11 +28,16 @@ $(".btn").click(function(){
 });
 
 function nextSequence(){
+
+    level++;
+
+    $("#level-title").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadein(100);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
     playSound(randomChosenColour);
 }
