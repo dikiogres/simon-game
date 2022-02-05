c6c14c3085249ab4624c7e061da5d1439913588f
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-02-05 21:09:17 +0700

    check the user's answer againts the game sequence

--- game.js
+++ game.js
@@ -22,12 +22,28 @@ $(".btn").click(function(){
     //console.log(userClickedPattern);
 
     playSound(userChosenColour);
-
     animatePress(userChosenColour);
 
+    checkAnswer(userClickedPattern.length-1);
 });
 
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        console.log("success!");
+
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000)
+        }
+    }else{
+        console.log("wrong!");
+    }
+}
+
 function nextSequence(){
+
+    userClickedPattern = [];
 
     level++;
 
