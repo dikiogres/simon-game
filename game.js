208dfa5aa5d26c8912cb4e5b106787d574b39a32
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-02-05 20:37:21 +0700

    feat: show the sequence to user with animations and sounds

--- game.js
+++ game.js
@@ -5,6 +5,10 @@ var gamePattern = [];
 function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
-
     gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadein(100);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
